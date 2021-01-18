ba35e90898e830fe4f35e0f1eeaffbaeeed2a9db
Nika Darab <nikadarab@pm.me>
2021-01-18 16:16:42 -0600

    post article

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -4,7 +4,7 @@ const supertest = require("supertest");
 const app = require("../src/app");
 const { makeArticlesArray } = require("./articles.fixtures");
 
-describe.only("Articles Endpoints", function () {
+describe("Articles Endpoints", function () {
   let db;
   before("make knex instance", () => {
     db = knex({
@@ -66,6 +66,7 @@ describe.only("Articles Endpoints", function () {
   });
   describe.only(`POST /articles`, () => {
     it(`creates an article, responding with 201 and the new article`, function () {
+      this.retries(3);
       const newArticle = {
         title: "Test new article",
         style: "Listicle",
@@ -80,12 +81,36 @@ describe.only("Articles Endpoints", function () {
           expect(res.body.style).to.eql(newArticle.style);
           expect(res.body.content).to.eql(newArticle.content);
           expect(res.body).to.have.property("id");
+          expect(res.headers.location).to.eql(`/articles/${res.body.id}`);
+          const expected = new Date().toLocaleString();
+          const actual = new Date(res.body.date_published).toLocaleString();
+          expect(actual).to.eql(expected);
         })
         .then((postRes) =>
           supertest(app)
             .get(`/articles/${postRes.body.id}`)
             .expect(postRes.body)
         );
+    });
+  });
+  const requiredFields = ["title", "style", "content"];
+
+  requiredFields.forEach((field) => {
+    const newArticle = {
+      title: "Test new article",
+      style: "Listicle",
+      content: "Test new article content...",
+    };
+
+    it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+      delete newArticle[field];
+
+      return supertest(app)
+        .post("/articles")
+        .send(newArticle)
+        .expect(400, {
+          error: { message: `Missing '${field}' in request body` },
+        });
     });
   });
 });
